60e7ec879e1af503d19fd301b4b021fbdc4e4a08
Timothy Moss <tjmoss@pm.me>
2022-12-20 11:29:49 -0500

    Paragraphs and subroutines tutorial

--- coboltut5.cob
+++ coboltut5.cob
@@ -10,5 +10,22 @@ FILE SECTION.
 WORKING-STORAGE SECTION.
 
 PROCEDURE DIVISION.
+SubOne.
+       DISPLAY "In paragraph 1"
+       PERFORM SubTwo
+       DISPLAY "Returned to Paragraph 1"
+       Perform SubFour 2 TIMES
+       DISPLAY "Repeat"
+       STOP RUN.
+SubThree.
+       DISPLAY "In paragraph 3".
+
+SubTwo.
+       DISPLAY "In paragraph 2"
+       PERFORM SubThree
+       DISPLAY "Returned to paragraph 2".
+
+SubFour.
+       DISPLAY "Repeat".
 
 STOP RUN.
