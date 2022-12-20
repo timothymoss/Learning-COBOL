d52521b4c722f6bb8f1196564f7886a4c50e5bd1
Timothy Moss <tjmoss@pm.me>
2022-12-20 12:38:13 -0500

    Fixed point integers tutorial

---
+++ coboltut9.cob
@@ -0,0 +1,19 @@
+       >>SOURCE FORMAT FREE
+IDENTIFICATION DIVISION.
+PROGRAM-ID. coboltut8.
+AUTHOR. Timothy Moss.
+DATE-WRITTEN. December 20th 2022
+ENVIRONMENT DIVISION.
+
+DATA DIVISION.
+FILE SECTION.
+WORKING-STORAGE SECTION.
+       01 Price PIC 9(4)V99.
+       01 TaxRate PIC V999 VALUE .075.
+       01 FullPrice PIC 9(4)V99.
+PROCEDURE DIVISION.
+       DISPLAY "Enter a price : " WITH NO ADVANCING.
+       ACCEPT Price
+       COMPUTE FullPrice ROUNDED = Price + (Price * TaxRate)
+       DISPLAY "Price + Tax : " FullPrice.
+STOP RUN.
