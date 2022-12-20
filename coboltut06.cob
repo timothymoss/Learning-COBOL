bbb23d2a8aea3a0bbf7d3dc2f515b90adab4bcc3
Timothy Moss <tjmoss@pm.me>
2022-12-20 13:05:57 -0500

    Renamed files and IDs for consistency

--- coboltut06.cob
+++ coboltut06.cob
@@ -1,6 +1,6 @@
        >>SOURCE FORMAT FREE
 IDENTIFICATION DIVISION.
-PROGRAM-ID. coboltut6.
+PROGRAM-ID. coboltut06.
 AUTHOR. Timothy Moss.
 DATE-WRITTEN. December 20th 2022
 ENVIRONMENT DIVISION.
@@ -12,7 +12,7 @@ WORKING-STORAGE SECTION.
        01 Num2       PIC 9 VALUE 4.
        01 Sum1       PIC 99.
 PROCEDURE DIVISION.
-CALL 'coboltut6getsum' USING Num1, Num2, Sum1.
+CALL 'coboltut06getsum' USING Num1, Num2, Sum1.
 DISPLAY Num1 " + " Num2 " = " Sum1
 
 STOP RUN.
